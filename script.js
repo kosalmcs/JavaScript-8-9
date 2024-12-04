f29631ff916fa288dcc99a44da36989a23cfd9b9
kosalmcs <94227040+kosalmcs@users.noreply.github.com>
2024-12-04 20:10:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+//Show Result
+
+// alert("Welcome");
+// console.log("Javascript");
+// document.write("Show Document");
+
+//variable
+// let x = 14;
+// // x = x + 2;
+// x *= 2;
+// x++2;
+
+// console.log(x);
+
+//Data Types
+// Double single int long date string
+
+// const cars = ["Saab", "Volvo", ""];
+// let car = cars[2];
+// console.log(car);
+
+//Object
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue",
+// };
+// console.log(person.eyeColor);
+// //Array
+// const persons = ["John", "Doe", 50, "blue", "white"];
+// console.log(persons[0]);
+
+//function
+//IvokeFunction (call Function)
+// function sum() {
+//   return 10 + 20;
+// }
+// //print (show result)
+// console.log(sum());
+
+//variable(var,let,const)
+
+//params
+// function salary(a, b, c) {
+//   return a + (b * 10) / 100 + c;
+// }
+// console.log(salary(1, 1, 10));
+// console.log(salary(10, 10, 2));
+// console.log(salary(5, 5, 5));
+
+//Object(Key/Value pair)
+
+// const car = {
+//   bmw: "BMW Car",
+//   highlander: "Highlander Car",
+//   lexus: "Lexus Car",
+// };
+// console.log(car.highlander);
+
+// const people = {
+//   name: "Thida",
+//   age: 24,
+//   arr: [1, 2, 3],
+// };
+// console.log(people.name + " " + people.arr[2]);
+
+// let arry = [
+//   { id: 1, name: "Moly", age: 24 },
+//   { id: 2, name: "Sok" },
+//   { id: 3, name: "Sao" },
+// ];
+// console.log(arry[1].id + arry[1].name + arry[1].age);
+//1000$
+// <=10 A (9.1-10)
+// <=9 B (8.1-9)
+// <=8 C
+// <=7 D
+// >=5 E
+// F
+// datatype
+//= Assign Value
+//== compare value
+//=== compare value with datatype
+const num1 = 12345; //datatype number
+const num2 = 12345; //datatype number
+
+if (num1 != num2) {
+  console.log("T");
+} else {
+  console.log("F");
+}
